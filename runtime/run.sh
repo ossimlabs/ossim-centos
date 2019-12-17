04cbe0665ef368696661822ff27c4e8fd79de73d
sbortman <scott.bortman@gmail.com>
2019-12-17 10:24:10 -0500

    added path to omar-services to test.   testing run-java.sh script as well

--- runtime/run.sh
+++ runtime/run.sh
@@ -6,4 +6,11 @@ DIST_DIR=$PWD/../dist
 mkdir -p $WORK_DIR
 mkdir -p $DIST_DIR
 
-docker run -it --rm -v $PWD:/scripts -v $WORK_DIR:/work -v $DIST_DIR:/usr/local -v $OSSIM_DATA:/data ossim-runtime-centos-minimal:local 
+docker run -it --rm \
+  -v $PWD:/scripts \
+  -v $WORK_DIR:/work \
+  -v $DIST_DIR:/usr/local \
+  -v $OSSIM_DATA:/data  \
+  -v $O2_DEV_HOME/omar-services/apps/omar-services-app:/omar-services \
+  -p 8081:8081 \
+  ossim-runtime-centos-minimal:local
