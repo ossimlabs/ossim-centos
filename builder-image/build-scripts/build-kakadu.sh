6b78eb7a1134225110063584558252fed2798373
randomtree07 <ryanfeldbush@gmail.com>
2020-06-04 15:34:05 -0400

    Changed kakadu location

--- builder-image/build-scripts/build-kakadu.sh
+++ builder-image/build-scripts/build-kakadu.sh
@@ -2,7 +2,7 @@
 
 export CXXFLAGS=-DKDU_NO_THREADS
 
-cd ossim-private/ossim-private/kakadu/v7_7_1-01123C
+cd ossim-private/kakadu/v7_7_1-01123C
 cd make
 make -f Makefile-Linux-x86-64-gcc
 #rm $(find . -name "*.so")
