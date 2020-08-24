57c9f6205d5da408715ef6a5e69b9f7721ea860f
Cory Robertson <coryr@Corys-MacBook-Pro.local>
2020-08-24 16:16:51 -0400

    trying something with QT paths

--- compile-ossim/build-scripts/build-ossim.sh
+++ compile-ossim/build-scripts/build-ossim.sh
@@ -18,14 +18,22 @@ rm -f $OSSIM_BUILD_DIR/CMakeCache.txt
 
 #
 #export VERBOSE=1
-export QTDIR=/usr/local/Trolltech/Qt-4.8.7
-export Qt4Core_DIR=$QTDIR/include/QtCore
-export Qt4OpenGL_DIR=$QTDIR/include/QtOpenGL
-export QT_MOC_EXECUTABLE=$QTDIR/bin
-export QT_RCC_EXECUTABLE:PATH=$QTDIR/bin
+export QTDIR=/usr/local/Trolltech/Qt-4.8.7/bin
+export Qt4Core_DIR=$QTDIR/../include/QtCore
+export Qt4OpenGL_DIR=$QTDIR/../include/QtOpenGL
+
+### the following three are missing
 #export QT_MOC_EXECUTABLE=$QTDIR/bin
+#export QT_RCC_EXECUTABLE=$QTDIR/bin
+#export QT_MOC_EXECUTABLE=$QTDIR/bin
+
+### someone somewhere told me to try this
 #export CMAKE_PREFIX_PATH=$QTDIR/bin
+
+### these two were here before, but idk how to transalte to current
 #export Qt4Widgets_DIR=$QTDIR/include/QtCore
+
+### this never helped
 #export QT_BINARY_DIR=/usr/local/Trolltech/Qt-4.8.7
 
 export BUILD_OSSIM_QT4=ON
